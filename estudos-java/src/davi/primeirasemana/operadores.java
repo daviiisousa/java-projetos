bb3acf687c0e7156fd8551075da712174d87daae
sousadavi248@gmail.com <sousadavi248@gmail.com>
2024-06-08 17:09:43 -0300

    operadores em java

---
+++ estudos-java/src/davi/primeirasemana/operadores.java
@@ -0,0 +1,13 @@
+package davi.primeirasemana;
+public class operadores {
+    public static void main(String[] args){
+        boolean numeroUm = true;
+        boolean numeroDois = true;
+
+        if(numeroUm && numeroDois){
+            System.out.println("deu bom");
+        }else{
+            System.out.println("deu ruim");
+        }
+    }
+}
