af1b7f1624ce702f1c601cdcce1a8bcf2ab14b1d
sousadavi248@gmail.com <sousadavi248@gmail.com>
2024-06-10 15:00:54 -0300

    paramentro via terminal

---
+++ java-terminal/src/SobreMim.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SobreMim {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Oii meu nome e " + nome + " " + sobrenome +  " e tenho" + idade + " anos");
+    }
+}
