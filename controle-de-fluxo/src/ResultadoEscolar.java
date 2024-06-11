73b43b4e96c9cd58cf98bb4465d7e18adb9ec81b
Davi <sousadavi248@gmail.com>
2024-06-11 20:53:03 -0300

    add condiçoes encadeadas

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,15 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 9;
+        int nota = 6;
 
-        if(nota < 6){
-            System.out.println("REPROVADO");
-        }else{
+        if(nota >= 6){
             System.out.println("APROVADO");
+        }else if(nota >= 5 && nota < 7){
+            System.out.println("RECUPERAÇAO");
+        }else{
+            System.out.println("REPROVADO");
         }
+        
 
     }
 }
