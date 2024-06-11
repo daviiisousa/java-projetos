4ee2e61a93cca0d9de1b4b9c9c39f54fd15bbd61
Davi <sousadavi248@gmail.com>
2024-06-11 20:22:16 -0300

    save

---
+++ controle-de-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,15 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 15;
+
+        if(numero1 < numero2){
+            System.out.println("eu errei");
+        }
+        
+            System.out.println(numero1);
+        
+            
+        
+    }
+}
