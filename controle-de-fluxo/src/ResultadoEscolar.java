e83661100aae68a0a9e8400fccf7f5676524ee66
Davi <sousadavi248@gmail.com>
2024-06-11 20:41:38 -0300

    criei o arquivo ResultadoEscolar

---
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,12 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 9;
+
+        if(nota < 6){
+            System.out.println("REPROVADO");
+        }else{
+            System.out.println("APROVADO");
+        }
+
+    }
+}
