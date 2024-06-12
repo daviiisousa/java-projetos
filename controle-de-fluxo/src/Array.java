cba34ecd6026811e5eedbc44e00a371f825fcfe3
Davi <sousadavi248@gmail.com>
2024-06-12 16:23:42 -0300

    aprendendo array e for em java

---
+++ controle-de-fluxo/src/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        String alunos[] ={"kaua", "josue", "davi", "pedro"};
+        for(String aluno: alunos){
+            System.out.println("o nome do aluno e " + aluno);
+        }
+    }
+}
