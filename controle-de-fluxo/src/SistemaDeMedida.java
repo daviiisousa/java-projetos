097e8a92d900aa6932519d478917e9c28d2cdab5
Davi <sousadavi248@gmail.com>
2024-06-11 21:20:35 -0300

    criei um switch case

---
+++ controle-de-fluxo/src/SistemaDeMedida.java
@@ -0,0 +1,24 @@
+public class SistemaDeMedida {
+    public static void main(String[] args) {
+        String sigla = "M";
+
+        switch (sigla) {
+            case "P":{
+                System.out.println("pequeno");
+                break;
+            }
+            case "M":{
+                System.out.println("medio");
+                break;
+            }
+            case "G":{
+                System.out.println("grande");
+                break;
+            }
+            default:
+                System.out.println("indefinido");
+                break;
+        }
+
+    }
+}
