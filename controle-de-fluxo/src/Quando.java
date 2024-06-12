96d1ece425087d0c0c83bf73fceefb54b167f492
Davi <sousadavi248@gmail.com>
2024-06-12 17:06:44 -0300

    aprendendo while

---
+++ controle-de-fluxo/src/Quando.java
@@ -0,0 +1,12 @@
+public class Quando {
+    public static void main(String[] args) {
+        int numero = 10;
+        while (numero < 11) {
+            
+            System.out.println("repetir");
+            numero = 11;
+            
+        }
+
+    }
+}
