ead68603ca6c309db9629d24c24003d367b97f17
Davi <sousadavi248@gmail.com>
2024-06-14 15:16:58 -0300

    criando exceçoes

--- java-terminal/src/SobreMim.java
+++ java-terminal/src/SobreMim.java
@@ -1,8 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class SobreMim {
     public static void main(String[] args) {
+        try{
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu nome");
@@ -15,5 +17,9 @@ public class SobreMim {
         int idade = scanner.nextInt();
 
         System.out.println("Oii meu nome e " + nome + " " + sobrenome +  " e tenho" + idade + " anos");
+        }
+        catch(InputMismatchException invalido){
+            System.out.println("voce prencheeu um campo errado, por favor refaça");
+        }
     }
 }
